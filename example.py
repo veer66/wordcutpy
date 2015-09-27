d2f69725a8d313fbe4b512d4618e24cf74215d47
Vee Satayamas <vsatayamas@gmail.com>
2015-09-28 00:52:04 +0700

    first commit

---
+++ example.py
@@ -0,0 +1,6 @@
+#! -*- coding: UTF8 -*-
+from wordcut import Wordcut
+if __name__ == '__main__':
+    wordcut = Wordcut(map(lambda w: w.rstrip().decode("UTF-8"), 
+                          open('dict.txt').readlines()))
+    print u"|".join(wordcut.tokenize(u"กากา cat หมา"))
