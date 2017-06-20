e940eea90d1d9babe20952e0109b2967f5e90c23
Praphan Klairith <xbird007@gmail.com>
2017-06-20 14:42:01 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='wordcutpy',
+      version='0.1',
+      description='wordcut python version',
+      url='https://github.com/veer66/wordcutpy.git',
+      author='Vee Satayamas',
+      author_email='',
+      license='MIT',
+      zip_safe=False)
