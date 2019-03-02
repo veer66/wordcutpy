5b9f30d08cece524e51bc3341b86a35d8114edf3
Vee Satayamas <vsatayamas@gmail.com>
2019-03-02 20:07:35 +0700

    Test wordcut with none as input

--- tests/test_wordcut.py
+++ tests/test_wordcut.py
@@ -5,6 +5,11 @@ sys.path.append(os.path.abspath('..'))
 from wordcut import Wordcut
 
 class TestWordcut(unittest.TestCase):
+    def setUp(self):
+        self.wordcut = Wordcut.bigthai()
+        
     def test_empty_input(self):
-        wordcut = Wordcut.bigthai()
-        self.assertEqual(wordcut.tokenize(""),[])
+        self.assertEqual(self.wordcut.tokenize(""),[])
+
+    def test_none_input(self):
+        self.assertEqual(self.wordcut.tokenize(None),[])
